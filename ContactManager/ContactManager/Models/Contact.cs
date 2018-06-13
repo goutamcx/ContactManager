2029caa75802dbaf620bf0dfc7a8fc02f8cf4c78
goutamcx <39689831+goutamcx@users.noreply.github.com>
2018-06-13 13:03:51 +0530

    Add files via upload
    ContactManager App 1st check-in

---
+++ ContactManager/ContactManager/Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        [StringLength(60, MinimumLength = 2)]
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [StringLength(60, MinimumLength = 2)]
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression(@"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$")]
+        [Display(Name = "e-mail")]
+        public string Email { get; set; }        
+        [Required]
+        [StringLength(12, MinimumLength = 3)]
+        public string Phone { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ ContactManager/ContactManager/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using Microsoft.EntityFrameworkCore;
+using ContactManager.Models;
+using ContactManager;
+
+namespace ContactManager
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
